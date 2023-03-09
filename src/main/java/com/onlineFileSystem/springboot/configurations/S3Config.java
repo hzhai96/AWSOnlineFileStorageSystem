d1a12d5bc7ce94536023555101549becb9c87d0a
hzhai96 <hzhai96@gmail.com>
2023-03-08 16:16:04 -0800

    Change aws basic credential provider to aws credential provider.

--- src/main/java/com/onlineFileSystem/springboot/configurations/S3Config.java
+++ src/main/java/com/onlineFileSystem/springboot/configurations/S3Config.java
@@ -2,7 +2,6 @@ package com.onlineFileSystem.springboot.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -12,10 +11,9 @@ public class S3Config {
 
     @Bean
     public S3Client client() {
-        // TODO: StaticCredentialsProvider should be replaced for security issue
         return S3Client.builder()
             .region(Region.US_EAST_1)
-            .credentialsProvider(StaticCredentialsProvider.create(config.credentials()))
+            .credentialsProvider(config.credentials())
             .build();
     }
 }
